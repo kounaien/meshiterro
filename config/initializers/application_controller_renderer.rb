59050d51101f3aaa3f2a2fb34a8ff034e9a8dee8
kounaien <1127fujihiroto@gmail.com>
2020-05-17 22:39:55 +0900

    start

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = '518880a01a48c6afe45eb7d115de92d4936eba7c4c135fde59db3a079a22405920cddccd841b87340fa779b3e2b24139b3953a7a5811b12674d7011842ffe5dd'
